e9e115fa9039c30a273eafa19c95a39d2613f21d
Alexey Zotov <alekseyzotov2001@gmail.com>
2022-03-03 22:31:55 +0200

    add a math command

---
+++ GuffiBot/Common/MathCmd.cs
@@ -0,0 +1,36 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Discord.Interactions;
+using GuffiBot.Log;
+using System.Data;
+
+namespace GuffiBot
+{
+    public class MathCmd : InteractionModuleBase<SocketInteractionContext>
+    {
+        public InteractionService Commands { get; set; }
+        private static Logger? _logger;
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public MathCmd(ConsoleLogger logger)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        {
+            _logger = logger;
+        }
+
+        [SlashCommand("math", "command math")]
+        public async Task MathAsync([Remainder] string math)
+        {
+            var dt = new DataTable();
+            var result = dt.Compute(math, null);
+
+            await ReplyAsync($":abacus: `Result:` **{result}**");
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+            await _logger.Log(new LogMessage(LogSeverity.Info, "MathModule : Math", $"User: {Context.User.Username}, Command: math", null));
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+
+        }
+    }
+}
+
