2f95180bc5fe1f513751a6e383ca46bdce786857
Alexey Zotov <alekseyzotov2001@gmail.com>
2022-03-03 22:40:43 +0200

    add kick command

--- GuffiBot/Common/Moderation/KickCmd.cs
+++ GuffiBot/Common/Moderation/KickCmd.cs
@@ -6,21 +6,21 @@ using GuffiBot.Log;
 
 namespace GuffiBot
 {
-    public class BanCmd : InteractionModuleBase<SocketInteractionContext>
+    public class KickCmd : InteractionModuleBase<SocketInteractionContext>
     {
         public InteractionService Commands { get; set; }
         private static Logger? _logger;
 
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-        public BanCmd(ConsoleLogger logger)
+        public KickCmd(ConsoleLogger logger)
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         {
             _logger = logger;
         }
 
-        [SlashCommand("ban", "command ban")]
-        [Discord.Commands.RequireUserPermission(GuildPermission.BanMembers, ErrorMessage = ":lock: You don't have the permission")]
-        public async Task BanMember(IGuildUser? user = null, [Remainder] string? reason = null)
+        [SlashCommand("kick", "command kick")]
+        [Discord.Commands.RequireUserPermission(GuildPermission.KickMembers, ErrorMessage = ":lock: You don't have the permission")]
+        public async Task KickMember(IGuildUser? user = null, [Remainder] string? reason = null)
         {
             if (user == null)
             {
@@ -28,23 +28,22 @@ namespace GuffiBot
                 return;
             }
             if (reason == null) reason = "Not specified!";
-            await Context.Guild.AddBanAsync(user, 1, reason);
+            await user.KickAsync(reason);
 
             var EmbedBuilder = new EmbedBuilder()
                 .WithColor(Color.Red)
-                .WithDescription($":white_check_mark: {user.Mention} was banned\n**Reason** {reason}")
+                .WithDescription($":shield: Moderator\n:white_check_mark: {user.Mention} was kicked\n**Reason** {reason}")
                 .WithFooter(footer =>
                 {
                     footer
-                    .WithText("User Ban Log")
+                    .WithText("User Kick Log")
                     .WithIconUrl("https://i.imgur.com/6Bi17B3.png");
                 });
             Embed embed = EmbedBuilder.Build();
             await ReplyAsync(embed: embed);
 #pragma warning disable CS8602 // Dereference of a possibly null reference.
-            await _logger.Log(new LogMessage(LogSeverity.Info, "BanModule : Ban", $"User: {Context.User.Username}, Command: ban", null));
+            await _logger.Log(new LogMessage(LogSeverity.Info, "KickModule : Kick", $"User: {Context.User.Username}, Command: kick", null));
 #pragma warning restore CS8602 // Dereference of a possibly null reference.
         }
     }
 }
-
